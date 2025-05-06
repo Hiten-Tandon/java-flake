9aa0b64691ad7acfb97408b41fa3fb3adb328f7a
Hiten Tandon <tandonhiten@gamil.com>
2025-05-06 20:48:22 +0530

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem(system: with import nixpkgs {inherit system;}; {
+    devShells.default = mkShell {
+      buildInputs = [ jetbrains.idea-community-src jdk23 ];
+    };
+
+    formatter = nixfmt-tree;
+  });
+}
